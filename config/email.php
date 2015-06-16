a7e9c7af6484f3ade66d88a0d049ec43fcaf367f
Victoria Moiseenko <moiseenkovika@gmail.com>
2015-06-16 16:54:01 -0700

    Update email.php

--- config/email.php
+++ config/email.php
@@ -2,7 +2,7 @@
 $config['protocol'] = 'smtp';
 $config['smtp_host'] = 'ssl://smtp.gmail.com';
 $config['smtp_user'] = 'moiseenkovika@gmail.com';
-$config['smtp_pass'] = 'Shellwedance99';
+$config['smtp_pass'] = 'password';
 $config['smtp_port'] = '465';
 $config['charset'] = 'utf-8';
 $config['newline'] = "\r\n";
